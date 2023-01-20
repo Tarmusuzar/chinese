52775c3ca7e753299dc622f7e3263d49a60f8fc0
Dev Suzar <93614295+Tarmusuzar@users.noreply.github.com>
2023-01-20 13:29:15 +0400

    Add main redirect

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 import MainSignin from './pages/MainSignin'
 import MainAdminproducts from './pages/MainAdminproducts.vue'
+import MainRedirect from './pages/MainRedirect.vue'
+
 
 import { createRouter,createWebHistory } from 'vue-router'
 
@@ -10,6 +12,7 @@ const router = createRouter({
     history:createWebHistory(),
     routes:[
         {path:'/admin',component:MainAdminproducts},
+        {path:'/dev',component:MainRedirect},
         {path:'/',component:MainSignin},
 
         
