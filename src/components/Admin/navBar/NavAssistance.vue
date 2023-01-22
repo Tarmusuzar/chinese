fe038791cb2862d55b03a6f6e7bd852919eb2654
Dev Suzar <93614295+Tarmusuzar@users.noreply.github.com>
2023-01-22 10:25:52 +0400

    Updating Assistance

--- src/components/Admin/navBar/NavAssistance.vue
+++ src/components/Admin/navBar/NavAssistance.vue
@@ -1,4 +1,24 @@
 
 <template>
-    <h1>Assistance</h1>
+    <div class="assistanceContainer">
+        <div>   
+                <i class="fa-solid fa-phone-volume"></i>
+                <p>Call Francis</p>
+                <p> 05478006354</p>
+        </div>      
+        
+         <div>   
+                <i class="fa-solid fa-envelope"></i>
+                <p>Email Francis</p>
+                <p> Tarmusuzar@gmail.com</p>
+        </div> 
+           <div>   
+                <i class="fa-solid fa-message-bot"></i>
+                <p>ETalk to a Rebot</p>
+                <p> Link here</p>
+        </div> 
+    </div>
 </template>
+<style scoped>
+
+</style>
